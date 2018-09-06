a51a094eec45ae006dd4f7053d1b81d8512bb4d2
MarionBarbee <35675226+MarionBarbee@users.noreply.github.com>
2018-09-05 23:14:22 -0500

    Add files via upload

--- t.bat
+++ t.bat
@@ -1,23 +1,23 @@
 copy p1.html webpuzzle.html
-solver
+Sudoku_solver
 copy p2.html webpuzzle.html
-solver
+Sudoku_solver
 copy p3.html webpuzzle.html
-solver
+Sudoku_solver
 copy p4.html webpuzzle.html
-solver
+Sudoku_solver
 copy p5.html webpuzzle.html
-solver
+Sudoku_solver
 copy p6.html webpuzzle.html
-solver
+Sudoku_solver
 copy p7.html webpuzzle.html
-solver
+Sudoku_solver
 copy p8.html webpuzzle.html
-solver
+Sudoku_solver
 copy p9.html webpuzzle.html
-solver
+Sudoku_solver
 copy p10.html webpuzzle.html
-solver
+Sudoku_solver
 
 
 
